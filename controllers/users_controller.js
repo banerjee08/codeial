234f27afab40c55be9a86354ec58983544a0b0a6
sumanto <banerjee.sumanto08@gmail.com>
2022-06-25 11:49:14 +0530

    sign-in created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -50,7 +50,27 @@ module.exports.create = function(req, res){
     
 }
 
-// sgn in and create a session for the user
+// sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+    // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in creating user while siging in');
+            return;
+        }
+        // handle user found
+        if(user){
+            // handle password which doesn't match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile')
+        }else{
+            // handle user not found
+            return res.redirect('back')
+        }
+    })
 }
