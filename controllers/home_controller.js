2d9b3fc152055c209d3d27fede1c1c57fa765f1a
sumanto <banerjee.sumanto08@gmail.com>
2022-07-14 10:30:42 +0530

    displaying posts on homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,20 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial</h1>')
 
     // requesting cookies from the browser and printing it in console
-    console.log(req.cookies);
+    // console.log(req.cookies);
     
     // changing the value of cookie
-    res.cookie('user_id', 25);
+    // res.cookie('user_id', 25);
     
-    return res.render('home', {
-        title: "Home"
+    // query to return all the posts
+    Post.find({}, function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        })
     })
 }
 
