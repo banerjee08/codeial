64593332b38652fdba9f201c17358f7cb7fa4e40
sumanto <banerjee.sumanto08@gmail.com>
2022-07-14 11:05:06 +0530

    displaying user of each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,11 +10,19 @@ module.exports.home = function(req, res){
     // res.cookie('user_id', 25);
     
     // query to return all the posts
-    Post.find({}, function(err, posts){
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     })
+    // })
+
+    // auto populate the user details of each post
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
-        })
+        });
     })
 }
 
